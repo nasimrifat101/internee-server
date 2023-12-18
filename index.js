ff1e45e9fde1ee6fa5f8d52855ad2f2e2127eece
nasimrifat101 <nasimrifat101@gmail.com>
2023-12-18 23:27:42 +0600

    feching user data

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yuxyuxp.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -25,6 +25,16 @@ async function run() {
     await client.connect();
 
     const userCollection = client.db("InterneeDB").collection("users");
+
+    // get user
+    app.get("/users", async (req, res) => {
+      try {
+        const result = await userCollection.find().toArray();
+        res.send(result);
+      } catch (err) {
+        console.log(err);
+      }
+    });
 
     // post user to database
     app.post("/users", async (req, res) => {
